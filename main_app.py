e3bb912d991849da248cab2583d4fdecdb6514ef
Shaan Gupta <ShaanGupta@Shaans-MacBook-Pro.local>
2022-07-13 13:34:51 +0530

    changed tenant

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ LOCAL = False
 class Config:
     
     if not LOCAL:
-        URL_PREFIX = ('https://salesdashboardtest-studio.dev.peak.ai/'
+        URL_PREFIX = ('https://sales-studio.dev.peak.ai/'
                       'app_direct/default')
     else:
         URL_PREFIX = ''
