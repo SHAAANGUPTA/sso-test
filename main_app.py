3bbdb35aa6320a205589f9950d35caee95400a59
Shaan Gupta <ShaanGupta@Shaans-MacBook-Pro.local>
2022-07-18 18:15:51 +0530

    changed tenant and environment

--- main_app.py
+++ main_app.py
@@ -7,7 +7,7 @@ LOCAL = False
 class Config:
     
     if not LOCAL:
-        URL_PREFIX = ('https://sales-adoptimization2605.beta.peak.ai/'
+        URL_PREFIX = ('https://sales-qatest.peak.ai/'
                       'app_direct/default')
     else:
         URL_PREFIX = ''
